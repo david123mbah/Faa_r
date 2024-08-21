3d40ecd4d9cca111965c601698f58cf6964e9222
Nde Dilan <nde.dilan@ictuniversity.edu.cm>
2024-08-21 16:09:30 +0100

    Adding the authorization, and middleware for some routes

--- app/(job)/contact/page.jsx
+++ app/(job)/contact/page.jsx
@@ -1,9 +1,17 @@
+"use client"
+import { useUser } from '@clerk/nextjs';
+import { redirect } from 'next/navigation';
 import React from 'react'
 
-const page = () => {
+const ContactPage = () => {
+  const { isLoaded, isSignedIn, user } = useUser();
+
+  if (!isSignedIn) {
+    redirect('/sign-in')
+  }
   return (
-    <div>page</div>
+    <div>ContactPage</div>
   )
 }
 
-export default page
+export default ContactPage
